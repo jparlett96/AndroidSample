a69764ff502cd4859671c8dfc543cb84ac955cfd
jparlett <jparlett@gmail.com>
2023-03-26 18:24:24 -0500

    Chapter 11 Code Android Studio

--- app/src/main/java/com/example/androidsample/MainActivity.java
+++ app/src/main/java/com/example/androidsample/MainActivity.java
@@ -6,38 +6,44 @@ import android.os.Bundle;
 
 import android.view.View;
 
-import android.widget.EditText;
+//import android.widget.EditText;
 
-import android.widget.TextView;
+//import android.widget.TextView;
 
 import java.util.Locale;
 
+import com.example.androidsample.databinding.ActivityMainBinding;
+
 public class MainActivity extends AppCompatActivity {
+
+    private ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-    }
 
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        View view = binding.getRoot();
+        setContentView(view);
+    }
 
     public void convertCurrency(View view) {
 
-        EditText dollarText = findViewById(R.id.dollarText);
+        //EditText dollarText = findViewById(R.id.dollarText);
 
-        TextView textView = findViewById(R.id.textView);
+        //TextView textView = findViewById(R.id.textView);
 
-        if (!dollarText.getText().toString().equals("")) {
+        if (!binding.dollarText.getText().toString().equals("")) {
 
-            float dollarValue = Float.parseFloat(dollarText.getText().toString());
+            float dollarValue = Float.parseFloat(binding.dollarText.getText().toString());
 
             float euroValue = dollarValue * 0.85F;
 
-            textView.setText(String.format(Locale.ENGLISH, "%f", euroValue));
+            binding.textView.setText(String.format(Locale.ENGLISH, "%f", euroValue));
 
         } else {
 
-            textView.setText(R.string.no_value_string);
+            binding.textView.setText(R.string.no_value_string);
 
         }
 
